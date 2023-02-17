8dc2a9346d4eb76cebd4b2c30493ee1763c3f755
Joel <moolick@outlook.com>
2023-02-17 14:31:50 -0600

    did some things

--- AStar/World/Map.cs
+++ AStar/World/Map.cs
@@ -37,34 +37,34 @@
             return Math.Abs(referenceTile.X - goalTile.X) + Math.Abs(referenceTile.Y - goalTile.Y);
         }
 
-        public IEnumerable<(Tile Neighbor, int Cost)> ExpandTile(Tile referenceTile)
+        public IEnumerable<(Tile Neighbor, int Cost)> ExpandTile(Tile referenceTile, int horizontalCost, int verticalCost)
         {
             if(referenceTile.X > 0)
             {
                 if (_tiles[referenceTile.X - 1][referenceTile.Y].Passable)
                 {
-                    yield return (_tiles[referenceTile.X - 1][referenceTile.Y], 4);
+                    yield return (_tiles[referenceTile.X - 1][referenceTile.Y], horizontalCost);
                 }
             }
             if (referenceTile.Y > 0)
             {
                 if (_tiles[referenceTile.X][referenceTile.Y - 1].Passable)
                 {
-                    yield return (_tiles[referenceTile.X][referenceTile.Y - 1], 3);
+                    yield return (_tiles[referenceTile.X][referenceTile.Y - 1], verticalCost);
                 }
             }
             if (referenceTile.X + 1 < Width)
             {
                 if (_tiles[referenceTile.X + 1][referenceTile.Y].Passable)
                 {
-                    yield return (_tiles[referenceTile.X + 1][referenceTile.Y], 4);
+                    yield return (_tiles[referenceTile.X + 1][referenceTile.Y], horizontalCost);
                 }
             }
             if (referenceTile.Y + 1 < Height)
             {
                 if (_tiles[referenceTile.X][referenceTile.Y + 1].Passable)
                 {
-                    yield return (_tiles[referenceTile.X][referenceTile.Y + 1], 3);
+                    yield return (_tiles[referenceTile.X][referenceTile.Y + 1], verticalCost);
                 }
             }
         }
