8905e12b2ee7cabd4c1871018d03719d3e2c5a19
Joel <moolick@outlook.com>
2023-02-18 02:41:53 -0600

    City testing

---
+++ AStar/World/City.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace AStar.World
+{
+    public class City
+    {
+        public string Name { get; init; }
+        public int HeuristicCost { get; init; }
+        public int PathCost { get; set; }
+
+        public bool IsExpanded { get; set; }
+        public bool OnFrontier { get; set; }
+        public bool IsNext { get; set; }
+
+        public List<(string Name, int Weight)> Neighbors { get; private set; }
+
+        public City(string name, int heuristicCost)
+        {
+            Name = name;
+            HeuristicCost = heuristicCost;
+            Neighbors = new List<(string Name, int Weight)>();
+        }
+    }
+}
